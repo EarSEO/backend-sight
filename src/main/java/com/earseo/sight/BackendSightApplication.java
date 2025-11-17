2ef8d0260f1d8152508d94689110536a4d6e7e88
yoonho <nam2660433@gmail.com>
2025-11-17 17:07:02 +0900

    Initial commit

---
+++ src/main/java/com/earseo/sight/BackendSightApplication.java
@@ -0,0 +1,13 @@
+package com.earseo.sight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSightApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/earseo/sight/common/exception/ErrorCodeInterface.java
@@ -0,0 +1,5 @@
+package com.earseo.sight.common.exception;
+
+public interface ErrorCodeInterface {
+    ErrorCode getErrorCode();
+}
